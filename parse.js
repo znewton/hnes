e560fc19797e33376e56b00c703f0b0674a8e5ab
znewton <znewton13@gmail.com>
2017-11-24 22:11:55 -0600

    Basic rendering

--- parse.js
+++ parse.js
@@ -10,46 +10,51 @@ function getPosts(page) {
     for (let p = 0; p < posts.length; ++p) {
         let post = posts.item(p);
         let info = post.nextElementSibling;
-        let voteLink = post.getElementsByClassName('votelinks')[0].getElementsByTagName('a')[0];
-        let storylink = post.getElementsByClassName('storylink')[0]
+        let title = post.getElementsByClassName('title')[0];
+        let voteLink = post.getElementsByClassName('votelinks')[0];
+        if (voteLink) voteLink = voteLink.getElementsByTagName('a')[0];
+        let storylink = post.getElementsByClassName('storylink')[0];
+        let sitestr = post.getElementsByClassName('sitestr')[0];
         let score = info.getElementsByClassName('score')[0];
         let user = info.getElementsByClassName('hnuser')[0];
-        let age = info.getElementsByClassName('age')[0].firstElementChild;
+        let age = info.getElementsByClassName('age')[0];
+        if (age) age = age.firstElementChild;
         let infoLinks = info.getElementsByTagName('a');
         let hide = infoLinks[infoLinks.length-2];
         let comments = infoLinks[infoLinks.length-1];
-        data.push({
-            title: post.getElementsByClassName('title')[0].textContent,
+        let thisPost = {
+            title: title ? title.textContent : '',
             story: {
-                text: storylink.textContent,
-                href: storylink.href,
+                text: storylink ? storylink.textContent : '',
+                href: storylink ? storylink.href : '',
                 site: {
-                    text: post.getElementsByClassName('sitestr')[0].textContent,
-                    href: post.getElementsByClassName('sitestr')[0].parentElement.getAttribute('href'),
+                    text: sitestr ? sitestr.textContent : '',
+                    href: sitestr ? sitestr.parentElement.getAttribute('href') : '',
                 }
             },
             vote: {
-                id: voteLink.id,
-                href: voteLink.href,
+                id: voteLink ? voteLink.id : voteLink,
+                href: voteLink ? voteLink.href : voteLink,
             },
             score: {
-                id: info.getElementsByClassName('score')[0].id,
-                points: parseInt(info.getElementsByClassName('score')[0].textContent.replace(/\D+/g, ''))
+                id: score ? score.id : score,
+                points: score ? parseInt(score.textContent.replace(/\D+/g, '')) : null
             },
             user: {
-                name: user.textContent,
-                href: user.href,
+                name: user ? user.textContent : '',
+                href: user ? user.href : '',
             },
             age: {
-                timeago: age.textContent,
-                href: age.getAttribute('href')
+                timeago: age ? age.textContent : '',
+                href: age ? age.getAttribute('href') : ''
             },
-            hideLink: hide.href,
+            hideLink: hide ? hide.href : '',
             comments: {
-                number: parseInt(comments.textContent.replace(/\D+/g, '')),
-                href: comments.href
+                number: comments ? parseInt(comments.textContent.replace(/\D+/g, '')) : null,
+                href: comments ? comments.href : ''
             }
-        });
+        };
+        data.push(thisPost);
     }
     return data;
 }
@@ -65,16 +70,16 @@ function buildPostHTML(post) {
 
     let title = document.createElement('div');
     title.className = 'title';
-    title.innerText = post.title;
+    title.innerText = post.title.substr(0,post.title.length - 1);
     element.appendChild(title);
 
-    let score = document.createElement('div');
+    let score = document.createElement('a');
     score.className = 'score';
-    let vote = document.createElement('a');
+    score.href = post.vote.href;
+    let vote = document.createElement('div');
     vote.className = 'upvote';
-    vote.href = post.vote.href;
     score.appendChild(vote);
-    let points = document.createElement('span');
+    let points = document.createElement('div');
     points.className = 'points';
     points.innerText = post.score.points;
     score.appendChild(points);
@@ -86,6 +91,25 @@ function buildPostHTML(post) {
     storyLink.innerText = post.story.text;
     storyLink.href = post.story.href;
     story.appendChild(storyLink);
+    let info = document.createElement('div');
+    info.className = 'info';
+    let timeago = document.createElement('a');
+    timeago.className = 'timeago';
+    timeago.innerText = post.age.timeago;
+    timeago.href = post.age.href;
+    info.appendChild(timeago);
+    info.appendChild(document.createTextNode(' by '));
+    let user = document.createElement('a');
+    user.className = 'user';
+    user.href = post.user.href;
+    user.innerText = post.user.name;
+    info.appendChild(user);
+    info.appendChild(document.createTextNode(' | '));
+    let comments = document.createElement('a');
+    comments.innerText = post.comments.number + ' comments';
+    comments.href = post.comments.href;
+    info.appendChild(comments);
+    story.appendChild(info);
     element.appendChild(story);
 
     let storySite = document.createElement('div');
@@ -108,7 +132,10 @@ for (let i = 0; i < headLinks.length; ++i) {
     }
 }
 document.body.innerHTML = '';
+let postList = document.createElement('main');
+postList.className = 'post-list';
 for (let i = 0; i < postData.length; ++i) {
     console.log(postData[i]);
-    document.body.appendChild(buildPostHTML(postData[i]));
+    postList.appendChild(buildPostHTML(postData[i]));
 }
+document.body.appendChild(postList);
